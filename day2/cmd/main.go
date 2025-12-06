16e8753a3e272d50d35e026eef54c5caedcccfbc
giufus <giufus@gmail.com>
2025-12-06 11:41:46 +0100

    day 2 1st part, to refactor

--- day2/cmd/main.go
+++ day2/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync/atomic"
 )
 
 
@@ -18,7 +19,8 @@ func Main(path string) int {
 	// Convert the byte slice to a string.
 	fileContent := string(content)
 	intervals := strings.Split(fileContent, ",")
-	twices := make(chan int)
+	var solution atomic.Int64
+	doneChan := make(chan struct{})
 	
 	for _, interval := range(intervals) {
 		intervalSlice := strings.Split(interval, "-")
@@ -28,18 +30,37 @@ func Main(path string) int {
 		if err != nil {
 			log.Println(err)
 		}
-		go func(s string, e string, t chan int) {
-			s, err := str.Atoi(s)
-			if err != null {
-				log.Println("not valid start")
-			}
-			for i:=s; i<=e; i++ {
-				
-			}
-		}(start, end, twices)
 
+		go func(s string, e string, sol *atomic.Int64) {
+			beginning, err := strconv.Atoi(s)
+			if err != nil {
+				log.Println("not valid beginning")
+			}
+			ending, err := strconv.Atoi(e)
+			if err != nil {
+				log.Println("not valid ending")
+			}
+			for i:=beginning; i<=ending; i++ {
+				backToString := fmt.Sprintf("%d", i)
+				if len(backToString) < 2 {
+					continue
+				}
+				firstHalf := backToString[0:len(backToString)/2]
+				secondHalf := backToString[len(backToString)/2:]
+				if firstHalf == secondHalf {
+					log.Printf("found an illegal %d\n", i)
+					sol.Add((int64)(i))
+				}
+			}
+			doneChan <- struct{}{}
+		}(start, end, &solution)
 	}
-	fmt.Scanln()
 
-	return 0
+	for i := 0; i < len(intervals); i++ {
+		<-doneChan
+	}
+
+	log.Printf("Solution 2 %v\n", solution.Load())
+
+	return int(solution.Load())
  }
--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	cmd1 "aoc2025/day1/cmd"
 	cmd2 "aoc2025/day2/cmd"
 	"fmt"
+	"os"
 	"path/filepath"
 )
 
 
 func main() {
-	path := filepath.Join("day1", "input")
-	fmt.Println("Solution is", cmd1.Main(path))
-	path = filepath.Join("day2", "input")
-	fmt.Println("Solution is", cmd2.Main(path))
+	switch {
+		case os.Args[1] == "1":
+			path := filepath.Join("day1", "input")
+			fmt.Println("Solution is", cmd1.Main(path))
+		case os.Args[1] == "2":
+			path := filepath.Join("day2", "input")
+			fmt.Println("Solution is", cmd2.Main(path))
+	}
 }
